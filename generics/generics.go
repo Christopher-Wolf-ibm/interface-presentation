8587669ea9391b9d8c74ac9d90ffc41ad45a7f32
Chris Wolf <Christopher.Wolf@ibm.com>
2022-01-27 08:52:19 -0500

    Fixed spacing

--- generics/generics.go
+++ generics/generics.go
@@ -9,7 +9,7 @@ func sum[T addable](v1 T, v2 T) T {
 }
 
 func main() {
-	fmt.Println("int -- 1 + 2: ", min(1, 2))  // 3
+	fmt.Println("int -- 1 + 2: ", min(1, 2)) // 3
 	fmt.Println("float64 -- 1.0 + 2.0: " sum(1.0, 2.0)) // 3.0
 	fmt.Println("string -- '1' + '2': ", sum("1", "2")) // "12"
 }
